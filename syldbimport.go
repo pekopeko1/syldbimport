7a654409811e33ed19d1125c95acb41823736223
pekopeko1 <t.moi.larsson+pekoepko1@gmail.com>
2015-10-30 00:08:05 +0900

    add folder_id

--- syldbimport.go
+++ syldbimport.go
@@ -1,7 +1,9 @@
 package main
 
 import (
-    "flag"
+	"flag"
+	"fmt"
+	"os"
 )
 
 var opt_dbname = flag.String("d", "", "database name")
@@ -10,20 +12,26 @@ var opt_port = flag.Int("p", 0, "port number of database server")
 var opt_username = flag.String("U", "", "username of database")
 var opt_password = flag.String("P", "", "password of database")
 var opt_mecab_encoding = flag.String("-mecab-encoding", "", "encoding of MeCab dictionary (default: Unix: EUC-JP / Win32: Shift_JIS)")
-var opt_dont_remove_messeges = flag.Bool("n", false, "don't remove nonexist messages")
+var opt_del_noexist = flag.Bool("n", false, "don't remove nonexist messages")
 var opt_recursive_import = flag.Bool("r", false, "recursive import")
 var opt_verbose = flag.Bool("v", false, "verbose output")
 var opt_exclude = flag.String("-exclude", "", "exclude foldername from import targets (can be specified multiple times)")
 var opt_delete = flag.Bool("-delete", false, "recursively delete folders from DB")
 var opt_debug = flag.Bool("-debug", false, "show debug console window")
 
-
 func main() {
-    flag.Parse()
-    // if command has no args, show help and exit
-    if flag.NFlag() == 0 {
-        flag.Usage()
-        return
-    }
+	var folder_id []string
 
+	flag.Parse()
+	// if command has no args, show help and exit
+	if flag.NFlag() == 0 {
+		flag.Usage()
+		return
+	}
+
+	folder_id = flag.Args()
+	if len(folder_id) == 0 {
+		fmt.Println("specify target folder.")
+		os.Exit(1)
+	}
 }
